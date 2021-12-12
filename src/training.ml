1b1ae5f6cdf6a48f6a5e2e778e4e0405a08ff05b
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-12-12 15:04:12 -0500

    Update training.ml

--- src/training.ml
+++ src/training.ml
@@ -11,18 +11,20 @@ module Dist = struct
 end 
 module Write_Distribution = Dist
 
-(* Pos_List_Map.sexp_of_t  *)
+let correct_directory _ = Sys.chdir("/Users/zhenboyan/Desktop/ocaml-final")
+
 let write_sexp_to_file filename distribution =
   let output_file = Out_channel.create ~append:false filename in
   Out_channel.output_string output_file (Write_Distribution.sexp_of_t distribution |> Sexp.to_string);
   Out_channel.close output_file 
 
 let sexp_to_map filename = 
-  Write_Distribution.t_of_sexp (Sexp.load_sexp filename);;
-  (* Pos_List_Map.t_of_sexp  *)
-let abc = Pos_grams.ngrams 2 1 [(0,1); (0,2); (0,3); (0,4); (0,5); (0,6); (0,7); (0,8); (0,9)]
+  Write_Distribution.t_of_sexp (Sexp.load_sexp filename)
 
-let () = write_sexp_to_file "abc.txt" abc
+let abc = Pos_grams.ngrams 2 1 [(0,1); (0,2); (0,3); (0,4); (0,5); (0,6); (0,7); (0,8); (0,9)]
+(* let dis = sexp_to_map "abc.txt" *)
+let () = correct_directory 1 ; write_sexp_to_file "abc.txt" abc;
+
 (* let abc = Pos_grams.ngrams 2 1 [ 1; 2; 3; 4; 5; 6; 7; 8; 9 ] *)
   
 (* change to correct directory *)
