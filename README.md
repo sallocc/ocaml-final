6aae3b3072d37baf65e7f3a91311bf281c595b6a
ZhenboYan <44856370+ZhenboYan@users.noreply.github.com>
2021-12-15 14:28:02 -0500

    c

--- README.md
+++ README.md
@@ -4,6 +4,14 @@ The repository for our Functional Programming and Software Engineering course
 
 Opam dependencies
 - Need to download dream. This can be done through an opam command "opam install dream.1.0.0~alpha2"
+
+Build and Test
+- Standard "dune build" to build the files
 - To execute the dream website run the command "dune exec --root . ./src/main.exe" in the top-level directory
 - If a dune-project file appears in the src folder, please delete before running tests as that will mess up the project dependencies
+- For testing, "dune test" runs the test suites (not including I/O functions)
+- For test coverage, first run "dune test" then "bisect-ppx-report html". To view the coverage report "open _coverage/index.html".
 
+Tranning AI
+- training.ml file contains the functions needed to train N-gram AI
+- After setting the parameters instructed in training.ml file, "dune exec ./src/training.exe" generates the AI and stored in the file given
--- src/training.ml
+++ src/training.ml
@@ -12,20 +12,20 @@ module Dist = struct
   type t = Pos_grams.distribution [@@deriving sexp]
 end
 
+(* for sexp writing *)
 module Write_Distribution = Dist
 
-let correct_directory _ = Sys.chdir "/Users/zhenboyan/Desktop/ocaml-final"
-
-let write_sexp_to_file filename distribution =
+let write_sexp_to_file (filename : string)
+    (distribution : Pos_grams.distribution) =
   let output_file = Out_channel.create ~append:false filename in
   Out_channel.output_string output_file
     (Write_Distribution.sexp_of_t distribution |> Sexp.to_string);
   Out_channel.close output_file
 
-let sexp_to_map filename =
+let sexp_to_map (filename : string) : Pos_grams.distribution =
   Write_Distribution.t_of_sexp (Sexp.load_sexp filename)
 
-let switch_player history =
+let switch_player (history : 'a list) =
   match List.length history % 2 with
   | 0 -> setPlayer 2
   | 1 -> setPlayer 1
@@ -38,13 +38,15 @@ let ai_trainning_move (dist : int -> int) (history : (int * int) list) :
     let m = dist 0 in
     let rec until_valid level move repeat =
       if level < 6 then
-        if level = 4 && repeat <> 20 then until_valid 0 (dist repeat) (repeat + 1)
+        if level = 4 && repeat <> 20 then
+          until_valid 0 (dist repeat) (repeat + 1)
         else if ai_is_valid_move (level, move) history then (level, move)
         else until_valid (level + 1) move repeat
       else until_valid 0 (dist 1) repeat
     in
     until_valid 0 m 0
 
+(* random moves for trainning purposes *)
 let all_0 _ : int = distribution_maker 94 1 1 1 1 1 1
 
 let all_1 _ : int = distribution_maker 1 94 1 1 1 1 1
@@ -58,25 +60,40 @@ let all_4 _ : int = distribution_maker 1 1 1 1 94 1 1
 let all_5 _ : int = distribution_maker 1 1 1 1 1 94 1
 
 let all_6 _ : int = distribution_maker 1 1 1 1 1 1 94
+
 let tail _ : int = distribution_maker 48 1 1 1 1 1 47
+
 let left _ : int = distribution_maker 30 36 30 1 1 1 1
-let right _ : int = distribution_maker 1 1 1 1 30 36 30 
+
+let right _ : int = distribution_maker 1 1 1 1 30 36 30
+
 let ll _ : int = distribution_maker 1 30 36 30 1 1 1
+
 let rr _ : int = distribution_maker 1 1 1 30 36 30 1
+
 let middle _ : int = distribution_maker 1 1 30 36 30 1 1
+
 let lll _ : int = distribution_maker 47 48 1 1 1 1 1
+
 let rrr _ : int = distribution_maker 1 1 1 1 1 48 47
 
-let empty_d = Pos_grams.ngrams 1 1 []
-
+(* number of games set to play *)
 let num_games = 500
 
-let random_d = all_4
+(* random distribution to train AI with*)
+let random_d = all_0
 
+(* number of times sample from distribution if it's invalid move *)
 let repeat = 15
 
-let vs_random store_file gram=
-  let rec play new_dist history i =
+(* random vs random, AI is not making moves only recording *)
+let vs_random (store_file : string) (gram : int) =
+  let rec play
+      (new_dist :
+        ( Pos_grams.Token_list_map.Key.t,
+          Pos.t Bag.t,
+          Pos_grams.Token_list_map.Key.comparator_witness )
+        Map_intf.Map.t) (history : Pos.t list) (i : int) =
     switch_player history;
     if i < num_games then
       match is_game_over (history_to_board history) history with
@@ -91,8 +108,14 @@ let vs_random store_file gram=
   in
   play (sexp_to_map store_file) [] 0
 
-let vs_itself store_file gram =
-  let rec play new_dist history i =
+(* AI vs AI, AI is making moves and recording *)
+let vs_itself (store_file : string) (gram : int) =
+  let rec play
+      (new_dist :
+        ( Pos_grams.Token_list_map.Key.t,
+          Pos.t Bag.t,
+          Pos_grams.Token_list_map.Key.comparator_witness )
+        Map_intf.Map.t) (history : Pos.t list) (i : int) =
     switch_player history;
     if i < num_games then
       match is_game_over (history_to_board history) history with
@@ -109,8 +132,14 @@ let vs_itself store_file gram =
   in
   play (sexp_to_map store_file) [] 0
 
-let train_player1 player1 gram =
-  let rec play new_dist history i =
+(* AI vs random, AI is player 1, player 2 is alwasy random with given distribution *)
+let train_player1 (player1 : string) (gram : int) =
+  let rec play
+      (new_dist :
+        ( Pos_grams.Token_list_map.Key.t,
+          Pos.t Bag.t,
+          Pos_grams.Token_list_map.Key.comparator_witness )
+        Map_intf.Map.t) (history : Pos.t list) (i : int) =
     switch_player history;
     if i < num_games then
       match is_game_over (history_to_board history) history with
@@ -130,11 +159,11 @@ let train_player1 player1 gram =
   in
   play (sexp_to_map "player1.txt") [] 0
 
-let rec n_times n = 
-  (train_player1 "player1.txt" n);
-  if n > 3 then n_times (n-1)  else ()
+(* number of grams to train from n to 4 *)
+let rec n_times (n : int) =
+  train_player1 "player1.txt" n;
+  if n > 3 then n_times (n - 1) else ()
 
 let () =
+  (* Sys.chdir "set to the right directory"; *)
   n_times 11
-  (* correct_directory "set to the right directory"; *)
-  
